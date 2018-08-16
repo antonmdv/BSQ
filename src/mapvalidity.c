ddfcad68f1c678b540f7617447d9ce424e2ae996
hryu96 <34956359+hryu96@users.noreply.github.com>
2018-08-15 22:49:38 -0700

    Update mapvalidity.c

--- src/mapvalidity.c
+++ src/mapvalidity.c
@@ -12,43 +12,41 @@
 
 #include "headers.h"
 
-int		mapvalidity(list *l)
+int	    mapvalidity(list *l)
 {
-    Matrix *mtrx;
-    int lineLength;
+    Matrix	*mtrx;
+	int		linelength;
 
-    lineLength = 0;
-    mtrx = malloc(sizeof(Matrix));
-
-    mtrx->numOfLines = l->numLines;
-    l = l->next;
-    if (mtrx->numOfLines <= 0)
-        return (0);
-    mtrx->emptyChar = l->c;
-    l = l->next;
-    mtrx->obstacle = l->c;
-    l = l->next;
-    mtrx->full = l->c;
-    l = l->next;
-
-    while (l->next)
-    {
-        if (l->c != '\n' && l->c != mtrx->emptyChar &&
-                    l->c != mtrx->obstacle && l->c != mtrx->full)
-            return (0);
-        if (lineLength == 0)
-        {
-            l = l->next;
-            lineLength = getlinelength(l);
-        }
-        else if (l->c == '\n')
-        {
-            l = l->next;
-            if(lineLength != getlinelength(l))
-                    return (0);
-        }
-        else
-            l =    l->next;
-    }
-    return (1);
+	lineLength = 0;
+	mtrx = malloc(sizeof(Matrix));
+	mtrx->numOfLines = l->numLines;
+	l = l->next;
+	if (mtrx->numOfLines <= 0)
+		return (0);
+	mtrx->emptyChar = l->c;
+	l = l->next;
+	mtrx->obstacle = l->c;
+	l = l->next;
+	mtrx->full = l->c;
+	l = l->next;
+	while (l->next)
+	{
+		if (l->c != '\n' && l->c != mtrx->emptyChar &&
+				l->c != mtrx->obstacle && l->c != mtrx->full)
+			return (0);
+		if (linelength == 0)
+		{
+			l = l->next;
+			linelength = getlinelength(l);
+		}
+		else if (l->c == '\n')
+		{
+			l = l->next;
+			if (linelength != getlinelength(l))
+				return (0);
+		}
+		else
+			l = l->next;
+	}
+	return (1);
 }
